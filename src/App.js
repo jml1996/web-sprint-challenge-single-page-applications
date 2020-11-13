6f4a6e367b5f1915b87406a432da5ee0a72701af
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2020-11-13 11:26:16 -0500

    Npm start not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Route, Link, Switch } from "react-router-dom";
 
 const App = () => {
+
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
+    // <div>
+    //   <Switch>
+    //     <Route path="/pizza">
+    //       <Pizza />
+    //     </Route>
+    //     <Route path="/">
+    //       <Form
+    //     </Route>
+    //   </Switch>
+    // </div>
   );
 };
 export default App;
